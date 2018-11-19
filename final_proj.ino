14048c63176c846ffba09e607f2f92f4440da94e
Joshua Anagbo <joshuaanagbo@Joshuas-MacBook-Pro-66.local>
2018-11-18 19:59:47 -0500

    Update final_proj.ino

--- final_proj.ino
+++ final_proj.ino
@@ -1,23 +1,22 @@
 int buttonPin[4] = {1, 2, 3, 4};
-int potPin[4] = {A8, A7, A6, A4};
+int potPin[4] = {A8, A7, A6, A5};
 //
 
 int potVal[4] = {0, 0, 0, 0};
 int lastPotVal[4] = {0, 0, 0, 0};
 int ccVal[4] = {0, 0, 0 , 0};
 int ccNumber[4] = {1, 2 , 3, 4};
-int channelNumber[4] = {1, 2, 3, 4};
 
 unsigned long lastCCTime[4] = {0, 0, 0, 0};
 
-boolean lastButtonStateArray[4] = {HIGH, HIGH, HIGH, HIGH};
+boolean lastButtonStateArray[4] = {LOW, LOW, LOW, LOW};
 boolean buttonStateArray[4] = {LOW, LOW, LOW, LOW};
 unsigned long lastButtonStateChange;
 
 void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(buttonPin[i], INPUT);
-  pinMode(potPin[i], INPUT);
+    //    pinMode(potPin[i], INPUT);
   }
   Serial.begin(9600);
 
@@ -25,58 +24,61 @@ void setup() {
 
 void loop() {
   usbMIDI.read();
-  buttonWork();
+  buttonsWork();
 }
 
-void buttonWork() {
+void buttonsWork() {
   for (int i = 0; i < 4; i++) {
+
+    if (digitalRead(buttonPin[i]) == HIGH) {
+
+      effectOn(i);
+    }
+
+
     if (millis() > lastButtonStateChange + 50) {
       lastButtonStateArray[i] = buttonStateArray[i];
       buttonStateArray[i] = digitalRead(buttonPin[i]);
 
-      if (buttonStateArray[i] == HIGH && lastButtonStateArray[i] == LOW) {
-        lastButtonStateChange = millis();
-        Serial.println("turn on");
-        effectOn();
-        //turn something on
-      } else if (buttonStateArray[i] == LOW  && lastButtonStateArray[i] == HIGH) {
+      if (buttonStateArray[i] == LOW  && lastButtonStateArray[i] == HIGH) {
         lastButtonStateChange = millis();
         //turn something off
-        effectOff();
+        effectOff(i);
         Serial.println("turn off");
 
       }
     }
-  }
 
-}
-
-void  effectOn() {
-
-  for (int i = 0; i < 4; i++) {
-
-    lastPotVal[i] = potVal[i];
-    potVal[i] = analogRead(potPin[i]);
-    if (potVal[i] != lastPotVal[i] && millis() > lastCCTime[i] || 0) {
-      lastCCTime[i] = millis();
-      ccVal[i] = map(potVal[i], 0, 1023, 0, 127);
-      usbMIDI.sendControlChange(ccNumber[i], ccVal[i], channelNumber[i]);
-      Serial.println(ccVal[i]);
-    }
   }
 }
 
-void  effectOff() {
+void  effectOn(int ccNum) {
 
-  for (int i = 0; i < 4; i++) {
+  Serial.println(ccNum);
 
-    lastPotVal[i] = potVal[i];
-    potVal[i] = analogRead(potPin[i]);
-    if (potVal[i] != lastPotVal[i] && millis() > lastCCTime[i] || 0) {
-      lastCCTime[i] = millis();
-      ccVal[i] = map(potVal[i], 0, 1023, 0, 127);
-      usbMIDI.sendControlChange(ccNumber[i], 0, channelNumber[i]);
-      Serial.println(ccVal[i]);
-    }
+
+  lastPotVal[ccNum] = potVal[ccNum];
+  potVal[ccNum] = analogRead(potPin[ccNum]);
+  if (potVal[ccNum] != lastPotVal[ccNum] && millis() > lastCCTime[ccNum] || 0) {
+    lastCCTime[ccNum] = millis();
+    ccVal[ccNum] = map(potVal[ccNum], 0, 1023, 0, 127);
+    usbMIDI.sendControlChange(ccNumber[ccNum], ccVal[ccNum], 1);
+    Serial.print(ccNum);
+    Serial.print(", ");
+    Serial.println(ccVal[ccNum]);
   }
+}
+
+
+
+void  effectOff(int ccNum) {
+
+
+  //  lastPotVal[ccNum] = potVal[ccNum];
+  //  potVal[ccNum] = analogRead(potPin[ccNum]);
+  //  if (potVal[ccNum] != lastPotVal[ccNum] && millis() > lastCCTime[ccNum] || 0) {
+  //    lastCCTime[ccNum] = millis();
+  //    ccVal[ccNum] = map(potVal[ccNum], 0, 1023, 0, 127);
+  usbMIDI.sendControlChange(ccNumber[ccNum], 0, 1);
+  //    Serial.println(ccVal[ccNum]);
 }
